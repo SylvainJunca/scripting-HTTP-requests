40b398c31b8714e930b942e4b1ae437d3a3cb464
SylvainJunca <sylvain.junca@gmail.com>
2018-10-29 13:25:19 -0400

    Fixes a typo in step 2

--- scripting-step02.js
+++ scripting-step02.js
@@ -4,7 +4,7 @@ function getAndPrintHTMLChunks () {
 
     var requestOptions = {
       host: 'sytantris.github.io',
-      path: '/http-examples/step1.html'
+      path: '/http-examples/step2.html'
     };
 
   https.get(requestOptions, function (response) {
